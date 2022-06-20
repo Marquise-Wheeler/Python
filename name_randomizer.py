cdea99d48fee587de88699a70720257d21c61fec
Marquise Wheeler <107084312+Marquise-Wheeler@users.noreply.github.com>
2022-06-20 00:32:09 -0500

    Create name_randomizer.py
    Code to create random unique names from user information.

---
+++ name_randomizer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# This progam will allow the user to input the number of unique names needed for the EC2 instances in their Environment.
+# After recieving the number the program will generate a unique name that includes the dept, 4 digits and 3 letters.
+
+# Import libraries
+import random
+import string
+
+# Assign.Initialize  variables
+letters = string.ascii_lowercase                                        # used to generate random lowercase strings.
+env_names = []                                                          # empty dictionary to store instance names.                     
+num = 0                                                                 # initialized variable to track iterations.
+instances = int(input('How many instances do you need names for? '))    # Prompt user input of # names.
+dept_name = input('What is the name of your department? ')              # Prompt for department name.              
+print(instances)                                                        # Output to verify user input
+
+# Begin while loop to create names.
+while (num < instances):
+    num = num + 1                                                       # Increment count by 1
+    unique_num = str(random.randrange(0000, 9999))                      # Change rand.range to string store in variable
+    unique_char = ''.join(random.sample(string.ascii_lowercase, 3))     # use .sample so string is not duplicated 
+    uniq_name = dept_name + unique_num + unique_char                    # concantenate strings to create unique name
+    env_names.append(uniq_name)                                         # Add names to the list.
+
+print(env_names)
+
+  
+# IDEAS TO IMPROVE THE CODE
+# Write the names to a file. If the name exists create a different name.
+# Print a list of names to use.
+
+# ADVANCED
+# Add validation so only allowd groups can use the code by querng the user as to which department they work in
+
+# COMPLEX
+# Turn the code into a function 
