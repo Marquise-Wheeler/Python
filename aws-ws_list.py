97ecc6ea1040bf5c62f56ac616b0436057bfa125
Marquise <mardanmw27+LUIT@gmail.com>
2022-06-21 23:04:09 +0000

    initial commit of aws-ws_list.py

---
+++ aws-ws_list.py
@@ -0,0 +1,17 @@
+#1/usr/bin/env Python
+
+# Lists
+
+fruit = ["apples", "oranges", "bananans"]
+fruit.append("strawberry")
+print(fruit)
+
+# Assigning [] to a variable
+fruit1 = []
+
+# using the list constructor
+fruit2 = list()
+
+
+print(len(fruit))
+print(fruit[-1])
