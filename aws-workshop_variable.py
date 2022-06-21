48d1b24dec5f03f2735471a6d297d7b463883a61
Marquise <mardanmw27+LUIT@gmail.com>
2022-06-21 12:51:06 +0000

    add aws_workshop.py and made changes to some_string.py

---
+++ aws-workshop_variable.py
@@ -0,0 +1,16 @@
+# Examples of strings
+
+a_str = " this is an example of a string in qoutes" # In python the word strin si abbreviated to str
+my_float = 5.5
+an_integer = 5 # integer is abbreviated to int
+shopping_list = ["apples", "grapes", "pears"]
+a_dict = {"userid": "JBloggs"} # dictionary is abbreviated to dict
+my_var = another_variable # variable is abbreviated to var
+test_function = my_function() # function is abbreviated to func
+example_tuple = ("apple", "orange", "pear")
+boolean_values = True # boolean is abbreviated to bool
+
+
+
+
+
--- some_string.py
+++ some_string.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env Python3.7
 
-some_string = "Hello Python"
+some_string = "Hello Python\n"
 print(some_string)
+
+some_string1 = "The quick brown fox jumped over the lazy dog"
+print(some_string1)
+some_string2 ='The quick brown fox jumped over the lazy dog\n'
+print(some_string2)
+
+first_name = "Monty"
+last_name = "Python"
+print(first_name+last_name)
+# print with space separating first and last name
+print(first_name+ " " + last_name)
+
+# Using variables in strings
+print("\nMy first name is {}. My family name is {}\n".format(first_name, last_name))
+
+print("Using f strings to include variables in your strings")
+firstname = "Marquise"
+lastname = "Wheeler"
+print(f"My first name is {firstname}. My family name is {lastname}")
