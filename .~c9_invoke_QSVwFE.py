6d1046c796cf81b81a28e2f0f06bf01eb645f3ec
Marquise <mardanmw27+LUIT@gmail.com>
2022-06-22 12:00:03 +0000

    made canges to list.py and added two repos also

---
+++ .~c9_invoke_QSVwFE.py
@@ -0,0 +1,9 @@
+#1/usr/bin/env Python
+
+# Lists
+
+fruit = ["apples", "oranges", "bananans"]
+print(fruit)
+
+# Assigning [] to a variable
+f
--- aws-ws_list.py
+++ aws-ws_list.py
@@ -2,7 +2,7 @@
 
 # Lists
 
-fruit = ["apples", "oranges", "bananans"]
+fruit = ["apples", "oranges", "bananas"]
 fruit.append("strawberry")
 print(fruit)
 
@@ -15,3 +15,39 @@ fruit2 = list()
 
 print(len(fruit))
 print(fruit[-1])
+print(fruit[-2])
+
+# Using insert
+
+fruit.insert(2, "kiwi")
+print(fruit)
+
+''' Organizing a list using the sorted function does 
+not change the original list '''
+print(sorted(fruit))
+
+# Sort method changes the original list
+fruit.sort()
+print(fruit)
+
+# print list in reverse order to undo issue the command again
+fruit.reverse()
+print(fruit)
+
+# Delete item from a list
+del fruit[1]
+print(fruit)
+
+# Using pop to store a deleted variable returns last item by default
+favorite_fruit = fruit.pop()
+print(favorite_fruit)
+
+# using pop with index to return a specific item
+fresh_fruit = fruit.pop(1)
+print(fresh_fruit)
+
+print(fruit)
+
+# removing an item by value using remove method
+fruit.remove('bananas')
+print(fruit)
