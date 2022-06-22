1dde10407e7b1364f9c422ad79e7f7eacef5a156
Marquise <mardanmw27+LUIT@gmail.com>
2022-06-22 13:55:18 +0000

    initial commit of lab1 hello world

---
+++ aws-ws_lab_1_hello_world.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env Python3
+
+# Date: 06/22/2022
+# A function that prints hello world
+
+#def hello_world():
+#    print('hello world')
+    
+# This line calls (runs) the function
+#hello_world()
+######### START NEW FUNCTION############
+# A function that returns hello world
+
+def hello_world():
+    return 'hello world'
+    
+#Assign the hello_world() function to a variable.
+greeting = hello_world()
+print(greeting)
