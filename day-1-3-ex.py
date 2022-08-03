922f26a7f6a2226530fb7ff3a617e2401f6b22ff
Marquise Wheeler <mardanmw27+LUIT@gmail.com>
2022-08-02 23:18:38 -0500

    Calc length of user input string

---
+++ day-1-3-ex.py
@@ -0,0 +1,8 @@
+# count the number of characcters in a users name.
+
+name = input("What is your name? ")
+
+print(len(name))
+
+# The code below produces the same prompt and answer.
+print(len(input("What is your name? ")))
