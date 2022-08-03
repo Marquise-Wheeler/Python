e4e8397df3b0d35e4ebf8c83b1f96749de1f74f9
Marquise Wheeler <mardanmw27+LUIT@gmail.com>
2022-08-02 22:44:42 -0500

    Day-1

---
+++ day-1-printing.py
@@ -0,0 +1,19 @@
+from traceback import print_list
+
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# new line character
+print("Hello world!\nHello world!")
+
+# concantenation
+print("Hello " + "Angela")
+print("Hello" + " Angela")
+print("Hello" + " " + "Angela")
+
+#using the input function
+# input() will get the user inppu in the console
+# Then print() will print th word "Hello" and the user
+print("Hello " + input("What is your name?") + "!")
