18de606a6689592bcbd100c3e8b79558afe84b2a
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2022-10-14 21:20:13 -0500

    convert_user_input.py

---
+++ 100Days_of_Code/convert_user_input.py
@@ -0,0 +1,9 @@
+# Take user input, separate digits assign the correct type and add the digits to get the sum
+two_digit_number = input("Enter a two digit number here: \n") #Prompt user for input
+
+print(two_digit_number[0])
+print(two_digit_number[1])
+
+first_digit = int(two_digit_number[0])                         #convert string to integer
+second_digit = int(two_digit_number[1])                        
+result = print(first_digit + second_digit)                     #Add integers
