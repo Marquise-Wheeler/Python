ce972152b2081eb326828be1f026acd9fb032064
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2022-10-14 20:34:41 -0500

    tip_calculator

---
+++ 100Days_of_Code/tip_calculator.py
@@ -0,0 +1,8 @@
+# This purpose of this rogram is to cacuate tips.
+
+print("Welocme to the tip calculator.")
+bill = input("What was the total bill?\n")
+friends = input("How many peope to split the bill?\n" )
+tip = Input("What percentage tip would you like to give?\n" )
+split = (((bill * tip) + bill) / 5)
+print("Each person should pay:" )
