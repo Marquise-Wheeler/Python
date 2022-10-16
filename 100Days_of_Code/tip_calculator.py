a5712646ced23ace9f83b21e372af24a74c67fa5
Marquise Wheeler <107084312+Marquise-Wheeler@users.noreply.github.com>
2022-10-16 17:34:48 -0500

    Update tip_calculator.py
    Changed the order of the lines of code and added comments.

--- 100Days_of_Code/tip_calculator.py
+++ 100Days_of_Code/tip_calculator.py
@@ -1,26 +1,18 @@
-# This purpose of this rogram is to cacuate tips.
+print("Welcome to the tip calculator.")
 
-
-#If the bill was $150.00, split between 5 people, with 12% tip. 
-
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
-
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
-
-print("Welocme to the tip calculator.")
-
-friends = input("How many people will split the bill?\n")
-tip = input("What percentage tip would you like to give? 10, 15, or 20%?\n" )
-tip_integer = int(tip) / 100
-#print(tip_integer)
+# User input
 bill = input("How much is the bill?\n" )
+tip = input("What percentage tip would you like to give? 10, 15, or 20%?\n" )
+friends = input("How many people will split the bill?\n")
+
+# Calculations
+tip_integer = int(tip) / 100
 total_bill = float(bill) * float(1 + tip_integer)
 friends_integer= int(friends)
 cost_person = total_bill / friends_integer
 format_cost_person = "{:.2f}".format(cost_person)
 
+# Print statement
 print(f"The total cost per person is ${format_cost_person}")
+
 # End of file
