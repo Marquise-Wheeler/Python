6aac7f1f022552a18db75bb7c7d542f8f88d9d50
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2022-10-16 16:29:42 +0000

    Add end of file comment

--- 100Days_of_Code/tip_calculator.py
+++ 100Days_of_Code/tip_calculator.py
@@ -6,3 +6,5 @@ friends = input("How many peope to split the bill?\n" )
 tip = Input("What percentage tip would you like to give?\n" )
 split = (((bill * tip) + bill) / 5)
 print("Each person should pay:" )
+
+# End of file
