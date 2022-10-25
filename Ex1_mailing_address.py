2659b51fa5e4ca6ea562e3842139b6e257cbfb89
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2022-10-25 00:02:08 +0000

    Display mailing adddress using variales and f string

---
+++ Ex1_mailing_address.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Ex1. Create a program that displays name and complete mailing address. Program does not need any user input.
+
+name = "Mickey Mouse\n"
+address = "123 Mickey Mouse Rd\n"
+city = "Disney, "
+state = "Florida "
+zip = "55555"
+print(f'{name}{address}{city}{state}{zip}')
