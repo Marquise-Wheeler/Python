d4f86a4043255d3c2e4a8b0a2cf8e7b3ffeecedb
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2023-12-20 06:46:25 -0600

    Correct print statement spelling

---
+++ bite_size_code/introduce-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# This app will ask classmates their name and a few questions about themselves.
+
+# Greeting
+print('Welcome back to school ! Answer these 3 questions to introduce yourself!')
+
+# Question 1
+name = input('What is your name?')
+print(name)
+
+# Question 2
+favorite_color = input('What is your favorite color?')
+print(favorite_color)
+
+# Question 3
+favorite_food = input('What is your favorite food?')
+print(favorite_food)
+
+# Quesion 4
+favorite_tv_show = input('What is your favorite TV show?')
+print(favorite_tv_show)
+
+# Question 5
+print(f"\nEveryone, meet {name}! {name}’s favorite color is {favorite_color}. {name}’s favorite food is {favorite_food}. {name}’s favorite TV show is {favorite_tv_show}.")
+
