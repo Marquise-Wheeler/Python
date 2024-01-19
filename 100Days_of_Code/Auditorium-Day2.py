2346c440d0c7e6f254b0d9b996a0f9c2899a4578
Marquise-Wheeler <mardanmw27+LUIT@gmail.com>
2024-01-19 09:17:08 -0600

    Checkin Auditorium-Day2.py

---
+++ 100Days_of_Code/Auditorium-Day2.py
@@ -0,0 +1,15 @@
+# The challenge to this code was to remove the issues that prevented execution like mixed quotes and indents
+#!/usr/bin/python3
+
+# Fix the code below 👇
+'''
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+'''
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
